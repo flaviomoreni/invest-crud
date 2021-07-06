175e1169e9fd6d901c6d1bd1fa6a8e7d100d6003
flaviomoreni <fmoreni@gmail.com>
2021-07-06 19:24:35 -0300

    adicionando exceptions

---
+++ src/br/com/fiap/investimento/exceptions/DadoDuplicadoException.java
@@ -0,0 +1,27 @@
+package br.com.fiap.investimento.exceptions;
+
+@SuppressWarnings("serial")
+public class DadoDuplicadoException extends Exception {
+
+	public DadoDuplicadoException() {
+	}
+
+	public DadoDuplicadoException(String message) {
+		super(message);
+	}
+
+	public DadoDuplicadoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DadoDuplicadoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DadoDuplicadoException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
---
+++ src/br/com/fiap/investimento/exceptions/DataBaseException.java
@@ -0,0 +1,26 @@
+package br.com.fiap.investimento.exceptions;
+
+@SuppressWarnings("serial")
+public class DataBaseException extends Exception {
+
+	public DataBaseException() {
+	}
+
+	public DataBaseException(String message) {
+		super(message);
+	}
+
+	public DataBaseException(Throwable cause) {
+		super(cause);
+	}
+
+	public DataBaseException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DataBaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
