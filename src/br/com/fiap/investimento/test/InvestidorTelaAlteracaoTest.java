d3c8544463facb40f66bb7ea3dcd991226e99d1e
flaviomoreni <fmoreni@gmail.com>
2021-07-06 21:58:23 -0300

    finalizando a tela de alteração

--- src/br/com/fiap/investimento/test/InvestidorTelaAlteracaoTest.java
+++ src/br/com/fiap/investimento/test/InvestidorTelaAlteracaoTest.java
@@ -1,22 +1,47 @@
 package br.com.fiap.investimento.test;
 
+import javax.swing.JOptionPane;
+
+import br.com.fiap.investimento.business.InvestidorBusiness;
+import br.com.fiap.investimento.exceptions.DadoInexistenteException;
+import br.com.fiap.investimento.exceptions.DadoNaoAlteradoException;
+import br.com.fiap.investimento.model.InvestidorPF;
+
 public class InvestidorTelaAlteracaoTest {
 
 	public static void main(String[] args) {
+		try {
+			// Dados digitados pelo usuário
+			int id = 1;
+			String cpf = "999.999.999-99";
+			String nome = "Alvaro Morata";
 
-		//Dados digitados pelo usuário
-		int id = 1;
-		String cpf = "999.999.999-99";
-		String nome = "Alvaro Morata";
-		
-		// Instanciar o objeto
-		
-		
-		// Executar o método do business para efeuar o Alteracao
-		
-		
-		// Exibir mensagem para o usuário
-		
+			// Instanciar o objeto
+			InvestidorPF invPF = new InvestidorPF();
+			invPF.setInvestidorId(id);
+			invPF.setCpf(cpf);
+			invPF.setNomeInvestidor(nome);
+
+			// Executar o método do business para efeuar o Alteracao
+			InvestidorBusiness business = new InvestidorBusiness();
+			business.alterarInvestidor(invPF);
+
+			// Exibir mensagem para o usuário
+			//JOptionPane.showMessageDialog(null, "Investidor alterado com sucesso com sucesso!");
+			System.out.println("Investidor alterado com sucesso com sucesso!");
+			
+		} catch (DadoNaoAlteradoException e) {
+			
+			//JOptionPane.showMessageDialog(null, "Ocorreu um erro na alteração dos dados!");
+			System.out.println("Ocorreu um erro na alteração dos dados!");
+			
+		} catch (DadoInexistenteException e) {
+			
+			//JOptionPane.showMessageDialog(null, "Os dados informados não existem no banco de dados");
+			System.out.println("Os dados informados não existem no banco de dados");
+			
+		}
+
 	}
 
 }
--- src/br/com/fiap/investimento/test/InvestidorTelaCadastroTest.java
+++ src/br/com/fiap/investimento/test/InvestidorTelaCadastroTest.java
@@ -1,21 +1,38 @@
 package br.com.fiap.investimento.test;
 
+import javax.swing.JOptionPane;
+
+import br.com.fiap.investimento.business.InvestidorBusiness;
+import br.com.fiap.investimento.exceptions.DadoNaoAlteradoException;
+import br.com.fiap.investimento.model.InvestidorPF;
+
 public class InvestidorTelaCadastroTest {
 
 	public static void main(String[] args) {
+		try {
+			
+			// Dados digitados pelo usuário
+			String cpf = "999.999.999-99";
+			String nome = "Alvaro Morata";
 
-		//Dados digitados pelo usuário
-		String cpf = "999.999.999-99";
-		String nome = "Alvaro Morata";
-		
-		// Instanciar o objeto
-		
-		
-		// Executar o método do business para efeuar o Cadastro
-		
-		
-		// Exibir mensagem para o usuário
-		
+			// Instanciar o objeto de entidade
+			InvestidorPF invPF = new InvestidorPF();
+			invPF.setCpf(cpf);
+			invPF.setNomeInvestidor(nome);
+
+			// Executar o método do business para efetuar o Cadastro
+			InvestidorBusiness business = new InvestidorBusiness();
+			business.cadastrarInvestidor(invPF);
+
+			// Exibir mensagem para o usuário
+			JOptionPane.showMessageDialog(null, "Investidor cadastrado com sucesso!");
+			
+		} catch (DadoNaoAlteradoException e) {
+
+			JOptionPane.showMessageDialog(null, "Não foi possível efetuar a alteração, por favor verifique os dados digitados!");
+			
+		}
+
 	}
 
 }
